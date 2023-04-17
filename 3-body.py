396cb2ac9bce1b7c4b6204079ec9f78a08e77775
pablogila <pablo.gila.herranz@gmail.com>
2023-04-18 01:09:34 +0200

    saving and reading initial conditions

--- 3-body.py
+++ 3-body.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation, PillowWriter
+import pandas as pd
+import os
 
 
 G = 6.674e-11
@@ -8,44 +10,79 @@ dt = 0.02
 time = 10
 t = np.arange(0, time, dt)
 max_mass = 10000000000000
-min_mass = 1000000000000
-velocity = 4
-distance = 10
-view = 30
+min_mass = 100000000000
+velocity = 10
+distance = 20
+#distance_far = 40 # The third body will be placed further away from the other two
+view = 50
+# If you want any specific initial conditions, enter them in the following file
+initial_conditions_file = 'initial.txt'
+title = 'Three Body Problem'
 
 
-x1, y1 = np.random.uniform(-distance, distance), np.random.uniform(-distance, distance)
-vx1, vy1 = np.random.uniform(-velocity, velocity), np.random.uniform(-velocity, velocity)
-m1 = np.random.uniform(min_mass, max_mass)
-
-x2, y2 = np.random.uniform(-distance, distance), np.random.uniform(-distance, distance)
-vx2, vy2 = np.random.uniform(-velocity, velocity), np.random.uniform(-velocity, velocity)
-m2 = np.random.uniform(min_mass, max_mass)
-
-x3, y3 = np.random.uniform(-distance, distance), np.random.uniform(-distance, distance)
-vx3, vy3 = np.random.uniform(-velocity, velocity), np.random.uniform(-velocity, velocity)
-m3 = np.random.uniform(min_mass, max_mass)
-
-
+# Empty arrays for initial positions and velocities
 pos = np.zeros((3, len(t), 2))
 vel = np.zeros((3, len(t), 2))
 
-pos[0, 0] = [x1, y1]
-vel[0, 0] = [vx1, vy1]
 
-pos[1, 0] = [x2, y2]
-vel[1, 0] = [vx2, vy2]
-
-pos[2, 0] = [x3, y3]
-vel[2, 0] = [vx3, vy3]
+path = os.path.dirname(os.path.realpath(__file__))
+init_file = os.path.join(path, initial_conditions_file)
 
 
-print('Initial positions and conditions:')
-print('x = {:.2f}, y = {:.2f}, vx = {:.2f}, vy = {:.2f}, m = {:.2e}'.format(x1, y1, vx1, vy1, m1))
-print('x = {:.2f}, y = {:.2f}, vx = {:.2f}, vy = {:.2f}, m = {:.2e}'.format(x2, y2, vx2, vy2, m2))
-print('x = {:.2f}, y = {:.2f}, vx = {:.2f}, vy = {:.2f}, m = {:.2e}'.format(x3, y3, vx3, vy3, m3))
+# Check if initial conditions file exists
+if initial_conditions_file in os.listdir(path):
+
+     print('Using initial conditions from file: ', initial_conditions_file)
+
+     initial_conditions = pd.read_csv(init_file, header=0).values.tolist()
+
+     pos[0, 0] = [initial_conditions[0][0], initial_conditions[0][1]]
+     vel[0, 0] = [initial_conditions[0][2], initial_conditions[0][3]]
+     m1 = initial_conditions[0][4]
+     
+     pos[1, 0] = [initial_conditions[1][0], initial_conditions[1][1]]
+     vel[1, 0] = [initial_conditions[1][2], initial_conditions[1][3]]
+     m2 = initial_conditions[1][4]
+     
+     pos[2, 0] = [initial_conditions[2][0], initial_conditions[2][1]]
+     vel[2, 0] = [initial_conditions[2][2], initial_conditions[2][3]]
+     m3 = initial_conditions[2][4]
+
+else:
+
+     print('Initial conditions were randomly set:')
+
+     x1, y1 = np.random.uniform(-distance, distance), np.random.uniform(-distance, distance)
+     vx1, vy1 = np.random.uniform(-velocity, velocity), np.random.uniform(-velocity, velocity)
+     m1 = np.random.uniform(min_mass, max_mass)
+
+     x2, y2 = np.random.uniform(-distance, distance), np.random.uniform(-distance, distance)
+     vx2, vy2 = np.random.uniform(-velocity, velocity), np.random.uniform(-velocity, velocity)
+     m2 = np.random.uniform(min_mass, max_mass)
+
+     x3, y3 = np.random.uniform(-distance, distance), np.random.uniform(-distance, distance)
+     vx3, vy3 = np.random.uniform(-velocity, velocity), np.random.uniform(-velocity, velocity)
+     m3 = np.random.uniform(min_mass, max_mass)
+
+     pos[0, 0] = [x1, y1]
+     vel[0, 0] = [vx1, vy1]
+
+     pos[1, 0] = [x2, y2]
+     vel[1, 0] = [vx2, vy2]
+
+     pos[2, 0] = [x3, y3]
+     vel[2, 0] = [vx3, vy3]
+
+     initial_conditions = [[x1,y1,vx1,vy1,m1],[x2,y2,vx2,vy2,m2],[x3,y3,vx3,vy3,m3]]
+
+     df = pd.DataFrame(initial_conditions, columns=['x', 'y', 'vx', 'vy', 'm'])
+     df.to_csv('initial_conditions.txt', header=True, index=False)
+
+for condition in initial_conditions:
+    print('x =', condition[0], ' y =', condition[1], ' vx =', condition[2], ' vy =', condition[3], ' m =', condition[4])
 
 
+# Calculate positions and velocities over time
 for i in range(1, len(t)):
     r1 = np.linalg.norm(pos[0, i-1] - pos[1, i-1])
     r2 = np.linalg.norm(pos[0, i-1] - pos[2, i-1])
@@ -67,7 +104,7 @@ for i in range(1, len(t)):
 fig, ax = plt.subplots()
 ax.set_xlim((-view, view))
 ax.set_ylim((-view, view))
-ax.set_title('Three Body Problem')
+ax.set_title(title)
 plt.axis('off')
 
 line1, = ax.plot([], [], 'r--', linewidth=0.5)
