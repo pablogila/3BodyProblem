30a57cb65de39128fd74935f4397f3b3a500b814
pablogila <pablo.gila.herranz@gmail.com>
2023-04-17 17:39:54 +0200

    suficiente por hoy

--- 3-body.py
+++ 3-body.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation, PillowWriter
 
+
 G = 6.674e-11
 dt = 0.02
 time = 10
@@ -11,6 +12,7 @@ min_mass = 1000000000000
 velocity = 4
 distance = 10
 view = 30
+
 
 x1, y1 = np.random.uniform(-distance, distance), np.random.uniform(-distance, distance)
 vx1, vy1 = np.random.uniform(-velocity, velocity), np.random.uniform(-velocity, velocity)
@@ -24,6 +26,7 @@ x3, y3 = np.random.uniform(-distance, distance), np.random.uniform(-distance, di
 vx3, vy3 = np.random.uniform(-velocity, velocity), np.random.uniform(-velocity, velocity)
 m3 = np.random.uniform(min_mass, max_mass)
 
+
 pos = np.zeros((3, len(t), 2))
 vel = np.zeros((3, len(t), 2))
 
@@ -36,10 +39,12 @@ vel[1, 0] = [vx2, vy2]
 pos[2, 0] = [x3, y3]
 vel[2, 0] = [vx3, vy3]
 
+
 print('Initial positions and conditions:')
 print('x = {:.2f}, y = {:.2f}, vx = {:.2f}, vy = {:.2f}, m = {:.2e}'.format(x1, y1, vx1, vy1, m1))
 print('x = {:.2f}, y = {:.2f}, vx = {:.2f}, vy = {:.2f}, m = {:.2e}'.format(x2, y2, vx2, vy2, m2))
 print('x = {:.2f}, y = {:.2f}, vx = {:.2f}, vy = {:.2f}, m = {:.2e}'.format(x3, y3, vx3, vy3, m3))
+
 
 for i in range(1, len(t)):
     r1 = np.linalg.norm(pos[0, i-1] - pos[1, i-1])
@@ -58,14 +63,11 @@ for i in range(1, len(t)):
     vel[:, i] = vel[:, i-1] + np.array([a1, a2, a3]) * dt
     pos[:, i] = pos[:, i-1] + vel[:, i] * dt
 
+
 fig, ax = plt.subplots()
 ax.set_xlim((-view, view))
 ax.set_ylim((-view, view))
-ax.set_xlabel('')
-ax.set_ylabel('')
 ax.set_title('Three Body Problem')
-ax.set_facecolor('black')
-
 plt.axis('off')
 
 line1, = ax.plot([], [], 'r--', linewidth=0.5)
